1d50411dee4579b99e6c02aca525e9d215e44dd8
haffiiorn <haffiiorn@gmail.com>
2015-04-18 14:44:10 +0000

    home.js
    Gera leikinn live, þeas leikmenn

--- src/js/controllers/home.js
+++ src/js/controllers/home.js
@@ -1,6 +1,55 @@
 app.controller("HomeController", ["$scope", "$location", "SocketService","PlayersService", function($scope, $location, SocketService,PlayersService) {
 	var socket = io.connect('10.42.104.61:3000/');
+         
+    $.ajax({
+            'url': 'http://10.42.104.61:3000/players',
+            'type': 'GET',
+            'success': function(data) {
+                console.log(data);
+                if(data.Players.length){
+                  console.log("Party and bullshit");
+                  console.log(data.Players[0]);
+                  $scope.player11 = data.Players[0].Name;
+                  $scope.player12 = data.Players[1].Name;
+                  $scope.player21 = data.Players[2].Name;
+                  $scope.player22 = data.Players[3].Name;
+                 $scope.$digest();
+                }
+                else{
+                  console.log("booo here");
+                  $scope.player11 = "Player 1";
+                  $scope.player12 = "Player 2";
+                  $scope.player21 = "Player 1";
+                  $scope.player22 = "Player 2";
+                  
+                  PlayersService.setCurrent(null);
+
+                  if(PlayersService.getPlayer11()){
+                    $scope.player11 = PlayersService.getPlayer11().Name;
+                     PlayersService.setCurrent(null);
+                  }
+                  if(PlayersService.getPlayer12()){
+                     $scope.player12 = PlayersService.getPlayer12().Name;
+                      PlayersService.setCurrent(null);
+                   }
+                  if(PlayersService.getPlayer21()){
+                    $scope.player21 = PlayersService.getPlayer21().Name;
+                    PlayersService.setCurrent(null);
+                  }
+                  if(PlayersService.getPlayer22()){
+                    $scope.player22 = PlayersService.getPlayer22().Name;
+                    PlayersService.setCurrent(null);
+                  }
+                 if(PlayersService.getPlayer11() && PlayersService.getPlayer12() && PlayersService.getPlayer21() && PlayersService.getPlayer22()){
+                   $('#startGame').show();
+                  }   
+                  $scope.$digest();         
+              } 
+            }
+        });  
+
     socket.emit('getScore');
+   /* if(!gameOn){
     $scope.player11 = "Player 1";
     $scope.player12 = "Player 2";
     
@@ -28,7 +77,11 @@ app.controller("HomeController", ["$scope", "$location", "SocketService","Player
         $('#startGame').show();
         
     }
-
+  }
+  else{
+    updateGame();
+      
+  }*/
     socket.on('goal1', function(score){
     $('#team1').text(score);
     });
@@ -47,57 +100,43 @@ app.controller("HomeController", ["$scope", "$location", "SocketService","Player
     $location.path("/statistics");
   };
 
+  function updateGame(){
+     $.ajax({
+            'url': 'http://10.42.104.61:3000/players',
+            'type': 'GET',
+            'success': function(data) {
+                console.log(data);
 
+            } 
+        });  
+  }
     $scope.createGame = function() {
       $("#startGame").hide();
-        var headers = {
-         'Content-Type': 'application/json',
-         'dataType':'json',
-         'Accept':'application/json',
-         'Authorization':'Basic RlVTOlNhbGFzYW5hMTIzNA=='
-        };
         var body = {
-            "team1user1":PlayersService.getPlayer11(),
-            "team1user2":PlayersService.getPlayer12(),
-            "team2user1":PlayersService.getPlayer21(),
-            "team2user2":PlayersService.getPlayer22()
+            "team1user1UserId": PlayersService.getPlayer11().UserId,
+            "team1user1Name" : PlayersService.getPlayer11().Name,
+            "team1user1ImagePath":PlayersService.getPlayer11().ImagePath,
+
+            "team1user2UserId": PlayersService.getPlayer12().UserId,
+            "team1user2Name" : PlayersService.getPlayer12().Name,
+            "team1user2ImagePath":PlayersService.getPlayer12().ImagePath,
+            
+            "team2user1UserId": PlayersService.getPlayer21().UserId,
+            "team2user1Name" : PlayersService.getPlayer21().Name,
+            "team2user1ImagePath":PlayersService.getPlayer21().ImagePath,
+
+            "team2user2UserId": PlayersService.getPlayer22().UserId,
+            "team2user2Name" : PlayersService.getPlayer22().Name,
+            "team2user2ImagePath":PlayersService.getPlayer22().ImagePath
         };
         $.ajax({
             'url': 'http://10.42.104.61:3000/start',
             'type': 'POST',
             'data': body,
             'success': function(response2) {
-                console.log(response2);
+                gameOn = true;
+                updateGame();
             } 
-        });
-       /* $.ajax({
-          'url': 'http://apprekdbs01.ad.acme.is:8000/Fussball_Project/tempGame.xsjs',
-          'type': 'POST',
-          'data': JSON.stringify(data),
-          'dataType': 'json',
-          'headers':headers,
-          'contentType': 'application/json; charset=utf-8',
-          'success': function(response) {
-              console.log(response);
-              response = JSON.parse(response);
-              var body = {
-                    gameOn:response.gameOn,
-                    gameId:response.gameId
-                };
-                $.ajax({
-                  'url': 'http://10.42.104.61:3000/start',
-                  'type': 'POST',
-                  'data': body,
-                  'success': function(response2) {
-                     console.log(response2);
-                    } 
-                });
-            },
-          'error' : function(request, status, error)
-          {
-            console.log("Status: "+status+" Error: " +error);
-          }
-        });*/
-     
+        });     
     };
 }]);
